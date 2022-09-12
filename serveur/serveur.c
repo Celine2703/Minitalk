dac53d91daf5675e1d3582d1d285cdda00e0a1b9
Celine Martin-Parisot <celine@LAPTOP-CELINE.localdomain>
2022-09-12 14:47:39 +0200

    show

--- serveur/serveur.c
+++ serveur/serveur.c
@@ -6,7 +6,7 @@
 /*   By: celine <celine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 16:58:07 by molich            #+#    #+#             */
-/*   Updated: 2022/09/12 10:51:26 by celine           ###   ########.fr       */
+/*   Updated: 2022/09/12 14:46:15 by celine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,30 @@ int main ()
 {
     sigset_t    set;
     struct sigaction    sig;
+    int nb;
+    char    str[100];
     c = 0;
     printf("PID = %u\n\n",getpid());
     sigemptyset(&set);
     sigaddset(&set, SIGUSR1);
     sig.sa_handler = handler;
-    int cpt = 50;
-    while (cpt --)
+    int cpt = 0;
+    c = 1;
+    while (c)
     {
         c = 0;
-        int nb = 8;
-        while (nb)
+        nb = 8;
+        while (nb--)
         {
             sigaction(SIGUSR1, &sig, NULL);
             sigaction(SIGUSR2, &sig, NULL);
             pause();
-            nb --;
         }
-        printf("%c", c);
+        printf("%c\n", c);
+        str[cpt++] = c;
     }
+    printf("%s\n", str);
+    exit(EXIT_SUCCESS);
 }
 
 void    handler(int signum)
