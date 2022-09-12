6144dc6ad4acec042a58fd7e10453377f2f498bc
Celine Martin-Parisot <celine@LAPTOP-CELINE.localdomain>
2022-09-12 10:52:47 +0200

    affichage caractere par caractere

--- client/client.c
+++ client/client.c
@@ -6,28 +6,41 @@
 /*   By: celine <celine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 17:10:00 by molich            #+#    #+#             */
-/*   Updated: 2022/09/11 12:47:24 by celine           ###   ########.fr       */
+/*   Updated: 2022/09/12 10:49:45 by celine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
+void    send_nbr(int pib, int nb, int cpt)
+{
+    if (cpt == 8)
+        return ;
+    else if (nb % 2 == 0)
+    {
+        send_nbr(pib, nb / 2, ++cpt);
+        usleep(130);
+        kill(pib, SIGUSR1);
+    }
+    else if (nb % 2 == 1)
+    {
+        send_nbr(pib, nb / 2, ++cpt);
+        usleep(130);
+        kill(pib, SIGUSR2);
+    }
+}
+
 int main(int argc, char **argv)
 {
+    int i;
+    int nb;
+
+    i = 0;
     if (argc != 3)
         return (0);
-    kill(atoi(argv[1]), SIGUSR2);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR1);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR1);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR1);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR1);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR2);
-    sleep(1);
-    kill(atoi(argv[1]), SIGUSR2);
-    sleep(1);
+    while (argv[2][i])
+    {
+        send_nbr(atoi(argv[1]), (int)(argv[2][i]), 0);
+        i++;
+    }
 }
--- serveur/serveur.c
+++ serveur/serveur.c
@@ -6,7 +6,7 @@
 /*   By: celine <celine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 16:58:07 by molich            #+#    #+#             */
-/*   Updated: 2022/09/11 12:43:22 by celine           ###   ########.fr       */
+/*   Updated: 2022/09/12 10:51:26 by celine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,19 @@ int main ()
     sigemptyset(&set);
     sigaddset(&set, SIGUSR1);
     sig.sa_handler = handler;
-    while (1)
+    int cpt = 50;
+    while (cpt --)
     {
-        sigaction(SIGUSR1, &sig, NULL);
-        sigaction(SIGUSR2, &sig, NULL);
-        pause();
-        printf("%c\n\n", c);
+        c = 0;
+        int nb = 8;
+        while (nb)
+        {
+            sigaction(SIGUSR1, &sig, NULL);
+            sigaction(SIGUSR2, &sig, NULL);
+            pause();
+            nb --;
+        }
+        printf("%c", c);
     }
 }
 
